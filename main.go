b49a4741de648e15c2076db7a8da8979095c7f2a
Dmiller20 <133711494+Dmiller20@users.noreply.github.com>
2024-02-16 00:14:09 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,111 +1,117 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	 "encoding/csv"
-	"os"
 	"log"
+	"os"
 	"github.com/gocolly/colly"
 )
 
-//takes a collector, instantiates callback functions for them
-	func PrintScraper(col *colly.Collector) {
 
-		col.OnRequest(func(r *colly.Request) {
+// takes a collector, instantiates callback functions for them
+func PrintScraper(col *colly.Collector) {
 
-			fmt.Println("Visiting: ", r.URL)
-		})
+	col.OnRequest(func(r *colly.Request) {
 
-		col.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Visiting: ", r.URL)
+	})
 
-			fmt.Println("Something went wrong: ", err)
-		})
 
-		col.OnResponse(func(r *colly.Response){
+	col.OnError(func(r *colly.Response, err error) {
 
-			fmt.Println("Page visited: ", r.Request.URL)
-		})
+		fmt.Println("Something went wrong: ", err)
+	})
 
-		col.OnHTML("a", func(e *colly.HTMLElement){
 
-			log.Println("%v", e.Attr("href"))
-		})
+	col.OnResponse(func(r *colly.Response) {
 
-		col.OnScraped(func(r *colly.Response){
-			fmt.Println(r.Request.URL, " Scraped!")
+		fmt.Println("Page visited: ", r.Request.URL)
+	})
 
-		})
+
+	col.OnHTML("a", func(e *colly.HTMLElement) {
+
+		log.Println("%v", e.Attr("href"))
+	})
+
+
+	col.OnScraped(func(r *colly.Response) {
+		fmt.Println(r.Request.URL, " Scraped!")
+
+	})
+}
+
+
+// fields of data we want to collect
+type WebProduct struct {
+	image, name, price string
+}
+
+
+func CSVFILE(wp []WebProduct, col *colly.Collector) {
+
+	col.OnHTML("li.product", func(e *colly.HTMLElement) {
+
+		webProd := WebProduct{}
+		fmt.Println("Gathering HTML Elements: ", e)
+		webProd.image = e.ChildAttr("img", "src")
+		webProd.price = e.ChildText(".price")
+		webProd.name = e.ChildText("h2")
+
+		//adds all the scraped data to the list of products
+		wp = append(wp, webProd)
+	})
+
+
+	file, err := os.Create("Scraper.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+
+	//file writer initialization
+	write := csv.NewWriter(file)
+
+
+	//defining CSV headers
+	headers := []string{
+
+		"image",
+		"name",
+		"price",
+	}
+
+
+	//write column headers
+	write.Write(headers)
+
+
+	//for loop to add each product to CSV output file
+	for _, web := range wp {
+
+		record := []string{
+			web.image,
+			web.name,
+			web.price,
+		}
+		write.Write(record)
 	}
 
 	
-	//fields of data we want to collect
-		type WebProduct struct{
-	
-			image, name, price string
-	
-		}
+	defer write.Flush()
+}
 
+func main() {
+	fmt.Println("Hello world!")
 
-		func CSVFILE( wp[] WebProduct, web WebProduct){
+	c := colly.NewCollector()
+	var webProduct []WebProduct
 
-				file, err := os.Create("Scraper.csv")
-				if err != nil{
-					log.Fatalln("Failed to create output CSV file", err)
-				}
-				defer file.Close()
+	c.Visit("https://scrapeme.live/shop/")
 
-				//file writer initialization
-				write := csv.NewWriter(file)
+	PrintScraper(c)
 
-				//defining CSV headers
-				headers := []string{
-
-					"image",
-					"name",
-					"price",
-				}
-
-				//write column headers
-				write.Write(headers)
-
-				//for loop to add each product to CSV output file
-				for _, web := range wp{
-
-					record := []string{
-						web.image,
-						web.name,
-						web.price,
-					}
-					write.Write(record)
-				}
-
-				defer write.Flush()
-		}
-
-
-
-
-	func main() {
-		fmt.Println("Hello world!")
-
-		c := colly.NewCollector()
-		var webProduct[] WebProduct
-
-		c.Visit("https://scrapeme.live/shop/")
-
-		PrintScraper(c)
-
-		c.OnHTML("li.product", func(e *colly.HTMLElement){
-			
-			webProd := WebProduct{}
-			fmt.Println("Gathering HTML Elements: ", e)
-			webProd.image = e.ChildAttr("img", "src")
-			webProd.price = e.ChildText(".price")
-			webProd.name = e.ChildText("h2")
-			
-			//adds all the scraped data to the list of products
-			webProduct = append(webProduct, webProd)
-			CSVFILE(webProduct, webProd)
-		})
-		
-	}
+	CSVFILE(webProduct, c)
+}
